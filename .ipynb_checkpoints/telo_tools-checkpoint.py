7ca51ef51bda776e98f01575083dc767df8541f8
Jared Luxton <jaredjamesluxton@gmail.com>
2019-09-23 20:17:08 -0600

    dev graph fxns

--- .ipynb_checkpoints/telo_tools-checkpoint.py
+++ .ipynb_checkpoints/telo_tools-checkpoint.py
@@ -52,6 +52,22 @@ def generate_dictionary_for_telomere_length_data(patharg):
     is associated with its respective individual telomere length data (VALUE) as a KEY:VALUE pair 
     in the dictionary. The dictionary can then be looped over to initialize all timepoint data
     for that individual for analysis, i.e visualizations, statistics, etc.
+    
+    Example function with types documented in the docstring.
+
+    `PEP 484`_ type annotations are supported. If attribute, parameter, and
+    return types are annotated according to `PEP 484`_, they do not need to be
+    included in the docstring:
+
+    Args:
+        param1 (int): The first parameter.
+        param2 (str): The second parameter.
+
+    Returns:
+        bool: The return value. True for success, False otherwise.
+
+    .. _PEP 484:
+        https://www.python.org/dev/peps/pep-0484/
     """
     
     # initialize dictionary to hold our data
@@ -70,30 +86,27 @@ def generate_dictionary_for_telomere_length_data(patharg):
             except:
                 print(f'{file.name} File issue loading..')
                 return -1
+
+            # grabbing individual telomere length data from the file 
+            individual_telos_lengths = df.iloc[:, 3]
             
             # these numbers correspond to rows containing information about the DAPI counterstain, NOT telomeres, so we drop
             DAPI_values_to_drop=[5, 192, 379, 566, 753, 940, 1127, 1314, 1501, 1688, 1875, 2062,
-                    2249, 2436, 2623, 2810, 2997, 3184, 3371, 3558, 3745, 3932, 4119, 4306, 4493, 
-                    4680, 4867, 5054, 5241, 5428]
-
-            # grabbing individual telomere length data from the file & dropping DAPI info
-            individual_telos_lengths = df.iloc[:, 3]
-            individual_telos_lengths = individual_telos_lengths.drop(labels=DAPI_values_to_drop)
+                                 2249, 2436, 2623, 2810, 2997, 3184, 3371, 3558, 3745, 3932, 4119, 
+                                 4306, 4493, 4680, 4867, 5054, 5241, 5428]
             
-            # first pass at generating synthetic data for github exposition; to initialize actual
-            # data, comment out the line below, and uncomment the .iloc[] line
-#             individual_telos_lengths = individual_telos_lengths.sample(2500, random_state=1)
+            # drop DAPI info, grab only telo measurements in the col
+            # enforce the telomere measurements as a numeric data type
+            individual_telos_lengths = individual_telos_lengths.drop(labels=DAPI_values_to_drop)
             individual_telos_lengths = individual_telos_lengths.iloc[7:5611]
-
-            # ensure the telomere measurements are a numeric data type, drop any missing values, 
-            # make data into a dataframe
             telos_str_toNaN = pd.to_numeric(individual_telos_lengths, errors='coerce')
+            
+            # drop any missing values, make data into a dataframe
             individual_telos_cleaned = telos_str_toNaN.dropna(axis=0, how='any')
             telos_df = individual_telos_cleaned.to_frame(name=None)
             
-            # remove any telomere measurements that lie beyond 3 standard deviations of the mean
-            # the data is relatively normal in shape, & this process removes about ~10-20 telos from ~5520
-            # modest loss, acceptable to help standardize
+            # remove telomere measurements/visual artifacts that lie beyond 3 standard deviations of the mean
+            # the data is relatively normal in shape, & this process removes about ~10-30 telos from ~5520
             telos_individ_df = telos_df[(np.abs(stats.zscore(telos_df)) < 3).all(axis=1)]
             file_name_trimmed = file.name.replace('.xlsx', '')
             dict_dfs[file_name_trimmed] = telos_individ_df
@@ -114,8 +127,8 @@ def make_dataframe_from_telomere_data_dict(telo_dict):
     for name_key, telo_value in telo_dict.items():
         id_num = name_key[3:7]
         time_point = get_timepoint(name_key)
-        telo_value = gen_missing_values_and_impute_or_randomsampledown(30, 184, pd.Series(telo_value.values.reshape(-1,)), 'rsamp')
-
+        telo_value = gen_missing_values_and_impute_or_randomsampledown(30, 184, pd.Series(telo_value.values.reshape(-1,)))
+        
         data.append([id_num, time_point, telo_value, np.mean(telo_value.values)])
 
     df = pd.DataFrame(data, columns = ['sample id', 'timepoint', 'telo data', 'telo means'])
@@ -156,34 +169,33 @@ def get_timepoint(name_key):
 
         
         
-def gen_missing_values_and_impute_or_randomsampledown(n_cells, telosPercell, astro_df, option=None):
+def gen_missing_values_and_impute_or_randomsampledown(n_cells, telosPercell, df):
     #if wanted to do for max. possible telomeres, just replace the subtraction with max telos
     # print('substracts second astro from first.. equalizing second to first')
 
-    if astro_df.size > 4600:
-        astro_dfsampled = astro_df.sample(4600)
-        return astro_dfsampled
+    if df.size > 5520:
+        df_sampled = df.sample(5520)
+        return df_sampled
 
-    if astro_df.size > 25 and astro_df.size <= 2300:
-        missing_data_difference = abs( (n_cells * telosPercell) - astro_df.size )
-        rsampled = astro_df.sample(missing_data_difference, replace=True, random_state=28)
-        concat_ed = pd.concat([rsampled, astro_df], sort=False)
+    if df.size > 25 and df.size <= 2760:
+        missing_data_difference = abs( (n_cells * telosPercell) - df.size )
+        sampled = df.sample(missing_data_difference, replace=True, random_state=28)
+        concat_ed = pd.concat([sampled, df], sort=False)
         np.random.shuffle(concat_ed.to_numpy())
         concat_ed.reset_index(drop=True, inplace=True)
         return concat_ed
 
-    if astro_df.size > 25 and astro_df.size < 4600:
-        missing_data_difference = abs( (n_cells * telosPercell) - astro_df.size )
-        if option == 'rsamp':
-            rsampled = astro_df.sample(missing_data_difference, random_state=28)
-            concat_ed = pd.concat([rsampled, astro_df], sort=False)
-            np.random.shuffle(concat_ed.to_numpy())
-            concat_ed.reset_index(drop=True, inplace=True)
-            return concat_ed
-        else:
-            return astro_df
+    if df.size > 25 and df.size < 5520:
+        missing_data_difference = abs( (n_cells * telosPercell) - df.size )
+        sampled = df.sample(missing_data_difference, random_state=28)
+        concat_ed = pd.concat([sampled, df], sort=False)
+        np.random.shuffle(concat_ed.to_numpy())
+        concat_ed.reset_index(drop=True, inplace=True)
+        return concat_ed
+
     else:
-        return astro_df
+        print(f'unable to standardize individual telomere counts.. please check your dataframe for errors')
+        return df
     
     
     
@@ -207,8 +219,6 @@ def calculate_apply_teloQuartiles_dataframe(df, ordered_timepoint_list):
     df['timepoint'].cat.set_categories(ordered_timepoint_list, inplace=True)
     df = df.sort_values(['sample id', 'timepoint']).reset_index(drop=True)
     
-    # cap
-    
     for i, row in df.iterrows():
 
         if ordered_timepoint_list[0] in row['timepoint']:
@@ -221,6 +231,15 @@ def calculate_apply_teloQuartiles_dataframe(df, ordered_timepoint_list):
         else:
             print('unknown label in row["timepoint"] of dataframe.. please check timepoint names')
             
+    return df
+
+
+
+def order_timepoint_col(df, ordered_timepoint_list):
+    df['timepoint'] = df['timepoint'].astype('category')
+    df['timepoint'].cat.set_categories(ordered_timepoint_list, inplace=True)
+    df = df.sort_values(['sample id', 'timepoint']).reset_index(drop=True)
+    
     return df
 
 
@@ -248,3 +267,63 @@ def explode_individual_telos(df):
     exploded_telos['individual telos'] = exploded_telos['individual telos'].astype('int64')
     
     return exploded_telos
+
+
+
+####################################################################################################
+
+# FUNCTIONS FOR GRAPHING INDIVIDUAL TELOMERE LENGTH DATA
+
+####################################################################################################
+
+
+# patient_ids = list(all_patients_df['patient id'].unique())
+# telo_mrp.histogram_plot_groups(x='telo data exploded', data=exploded_telos_all_patients_df, groupby='patient id', iterable=patient_ids)
+
+def histogram_plot_groups(x=None, df=None, sample_id_col=None, groupby=None, num_samps_per_group=None):
+    
+    iterable = list(df[sample_id_col].unique())
+    group_df = df.groupby(groupby)
+    
+    for item in iterable:
+        plot_df = group_df.get_group(item)
+        timepoint_telo_values_dict = {}
+        
+        for timepoint in plof_df[sample_id_col]:
+            timepoint_telo_values_dict[sample_id] = plot_df[plot_df['timepoint'] == timepoint][x] 
+        
+        if num_samps_per_group == 2:
+        
+            non_irrad = plot_df[plot_df['timepoint'] == '1 non irrad'][x]
+            irrad_4_Gy = plot_df[plot_df['timepoint'] == '2 irrad @ 4 Gy'][x]
+            three_B = plot_df[plot_df['timepoint'] == '3 B'][x]
+            four_C = plot_df[plot_df['timepoint'] == '4 C'][x]
+
+            n_bins = 60
+            fig, axs = plt.subplots(2, 2, sharey=True, sharex=True, constrained_layout=True, figsize = (14, 9))
+
+            ax = sns.set_style(style="darkgrid",rc= {'patch.edgecolor': 'black'})
+    #         ax = sns.set(font_scale=1.4)
+
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, non_irrad, non_irrad, f'patient #{item} 1 non rad', 0, 0)
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, irrad_4_Gy, non_irrad, f'patient #{item} 2 irrad @ 4 Gy', 0, 1)
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, three_B,  non_irrad, f'patient #{item} 3 B', 1, 0)
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, four_C,  non_irrad, f'patient #{item} 4 C', 1, 1)
+            
+        elif num_samps_per_group == 4:
+        
+            non_irrad = plot_df[plot_df['timepoint'] == '1 non irrad'][x]
+            irrad_4_Gy = plot_df[plot_df['timepoint'] == '2 irrad @ 4 Gy'][x]
+            three_B = plot_df[plot_df['timepoint'] == '3 B'][x]
+            four_C = plot_df[plot_df['timepoint'] == '4 C'][x]
+
+            n_bins = 60
+            fig, axs = plt.subplots(2, 2, sharey=True, sharex=True, constrained_layout=True, figsize = (14, 9))
+
+            ax = sns.set_style(style="darkgrid",rc= {'patch.edgecolor': 'black'})
+    #         ax = sns.set(font_scale=1.4)
+
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, non_irrad, non_irrad, f'patient #{item} 1 non rad', 0, 0)
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, irrad_4_Gy, non_irrad, f'patient #{item} 2 irrad @ 4 Gy', 0, 1)
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, three_B,  non_irrad, f'patient #{item} 3 B', 1, 0)
+            histogram_stylizer_divyBins_byQuartile(fig, axs, n_bins, four_C,  non_irrad, f'patient #{item} 4 C', 1, 1)
